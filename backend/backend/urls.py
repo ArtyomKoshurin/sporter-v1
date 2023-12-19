2d3425f673453fd5c20d5bcfc19453b03b863041
Михаил Марин <mmn-83@yandex.ru>
2023-12-19 11:45:27 +0300

    Исправлен namespace в events

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,7 +7,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls', namespace='users')),
-    path('api/', include('events.urls', namespace='urls'))
+    path('api/', include('events.urls', namespace='events'))
 ]
 
 if settings.DEBUG:
